97471cc7e98b5edffbb395f9f141de7303e7676f
CedricLeon <cedric.leonard@insa-rennes.fr>
2021-11-23 11:15:05 +0200

    Ex3: Pattern analyze + algorithm implementation (mm.c)

--- Ex3/Exercise3.md
+++ Ex3/Exercise3.md
@@ -1,22 +1,45 @@
-# CODE OPTIMIZATION - EXERCISE 3
+# Code Optimization: Assignment N°3 Report
 
-   1. Matrix multipication algorithm
-    a. Analyze the memory acccess patterns 
-    **for (i,k,j) orders:**
-  - Access to elements in matrix b are done row-wise as similar to an example. Hence, for every 8 iteration, there will be a cache miss 
-- Access to elements in matrix c has the same cache miss as matrix b since it's a row-wise operation. 
-In total, there's 0.25 cache miss in the innermost loop after each iteration
-    
-    **for (j,k,i) orders**
-    Access to the elements in matrix a and c are done collumn-wise, which will lead to a cache miss on every iteration. The total cache miss of each iteration in the innermost loop is 2
+###### Duy Le - Cédric Léonard, 23/11/2021
 
-    
-   b. 
-   c. ?
+### Introduction
 
+This third assignment intends to dive deeper into the Memory system and optimization. The goal of this exercise is to manipulate and test 3 version of the basic matrix multiplication algorithm and then to write a new algorithm with cache blocking.
 
-2. Matrix multiplation with cache blocking
-   a. 
-   b. A block size should has 4 values for L1 (4x8=32) and 128 for L2(128x8=1024)
+### Question N°1
 
-3. 
+##### a. Analyze the memory access patterns
+
+In this part we will consider that a cache line is made of 8 consecutive values and when talking about accesses we will talk about accesses inside the inner-most for loop.
+
+###### (i, j, k) or (j, i, k) orders
+
+In this most straight forward implementation, matrix $A$ accesses are done row-wise and matrix $B$ accesses are done column-wise. This leads to a cache miss for matrix $A$ every 8 accesses and a miss each iteration for matrix $B$ (except if the matrix size is $< 8$ which is unlikely).
+
+As explained in the subject, this leads to a $1.125$ cache misses per iteration.
+
+###### (k, i, j) or (i, k, j) orders
+
+In this second implementation: access to elements in matrix $B$ are done row-wise, as similar to an example. Hence, for every 8 iteration, there will be a cache miss. Access to elements in matrix $C$ has the same cache miss as matrix $B$ since it's a row-wise operation. 
+In total, there is $0.25$ cache miss in the innermost loop after each iteration.
+
+###### (j, k, i) or (k, j, i) orders
+
+In this last implementation access to the elements in matrix $A$ and $C$ are done column-wise, which will lead to a cache miss on every iteration. The total cache miss of each iteration in the innermost loop is $2$.
+
+##### b. Implement and test the algorithms
+
+##### c. Results verification
+
+### Question N°2
+
+##### a. Implement a blocked version of matrix multiplication
+
+##### b. Calculate block size
+
+A block size should has 4 values for L1 (4x8=32) and 128 for L2(128x8=1024)
+
+##### c. Test and determine best block size
+
+### Question N°3
+
--- Ex3/mm.c
+++ Ex3/mm.c
@@ -83,28 +83,60 @@ int fwrite_matrix(double **M, const int rows, const int cols, char *fn, float ti
   Multiply the matrices A and B and place the result in C.
   The matrices are assumed to be square of size N*N
 */
-void matrixmult(double **A, double **B, double **C, const int N)
+/* (i, j, k) order */
+void matrixmult_ijk(double **A, double **B, double **C, const int N)
 {
-  // For each row
-  for(int r = 0; r < N; r++)
+  for(int i = 0; i < N; i++)      // For each row
   {
-    // For each column
-    for(int c = 0; c < N; c++)
+    for(int j = 0; j < N; j++)    // For each column
     {
-      // Init C coefficient
-      C[r][c] = 0;
-      // Add each A and B coefficients multiplication
-      for(int k = 0; k < N; k++)
-        C[r][c] += A[r][k] * B[k][c];
+      double sum = 0;             // Init local sum
+      for(int k = 0; k < N; k++)  // Add each A and B coefficients multiplication
+        sum += A[i][k] * B[k][j];
+      C[i][j] = sum;
     }
   }
 }
+/* (i, k, j) order */
+void matrixmult_ikj(double **A, double **B, double **C, const int N)
+{
+  for(int i = 0; i < N; i++)
+  {
+    for(int k = 0; k < N; k++)
+    {
+      double r = A[i][k];
+      for(int j = 0; j < N; j++)
+        C[i][j] += r * B[k][j];
+    }
+  }
+}
+/* (j, k, i) order */
+void matrixmult_jki(double **A, double **B, double **C, const int N)
+{
+  for(int j = 0; j < N; j++)
+  {
+    for(int k = 0; k < N; k++)
+    {
+      double r = B[i][k];
+      for(int i = 0; i < N; i++)
+        C[i][j] += A[i][k] * r;
+    }
+  }
+}
+
+void matrixreset(double **M)
+{
+  for(int i = 0; i < N; i++)
+    for(int j = 0; j < N; j++)
+      M[i][j] = 0;
+}
+
 
 
 int main(int argc, char** argv)
 {
   int size = 1000;          // Default matrix size is 1000*1000
-  clock_t start1;
+  clock_t start1, start2, start3;
 
   // Check if a matrix size is given on the command line
   if (argc > 1)
@@ -114,10 +146,9 @@ int main(int argc, char** argv)
   double **A = allocate_matrix(size, size);
   double **B = allocate_matrix(size, size);
   double **C = allocate_matrix(size, size);
-  
   printf("Allocated matrices of size %d\n\n", size);
 
-  // Initilaize the matrices with some values
+  // Initialize the matrices with some values
   for (int i = 0; i < size; i++)
   {
     for (int j = 0; j < size; j++)
@@ -128,11 +159,27 @@ int main(int argc, char** argv)
     }
   }
 
-  start1 = clock();    /* Start measuring time */
-  // Do the matrix multiplication
-  matrixmult(A, B, C, size);
-  float time = ((clock() - start1) / 1000000.0);
-  printf("Time for matrix multiplication %6.2f seconds\n", time);
+  // Test (i, j, k) order algorithm
+  start1 = clock();                                   // Start measuring time
+  matrixmult_ijk(A, B, C, size);                      // Do the matrix multiplication
+  float time = ((clock() - start1) / 1000000.0);      // Compute time
+  printf("IJK: Time for matrix multiplication %6.2f seconds\n", time);
+  // Reset matrix C
+  matrixreset(C);
+
+  // Test (i, k, j) order algorithm
+  start2 = clock();                                   // Start measuring time
+  matrixmult_ikj(A, B, C, size);                      // Do the matrix multiplication
+  float time = ((clock() - start2) / 1000000.0);      // Compute time
+  printf("IKJ: Time for matrix multiplication %6.2f seconds\n", time);
+  // Reset matrix C
+  matrixreset(C);
+
+  // Test (j, k, i) order algorithm
+  start3 = clock();                                   // Start measuring time
+  matrixmult_jki(A, B, C, size);                      // Do the matrix multiplication
+  float time = ((clock() - start3) / 1000000.0);      // Compute time
+  printf("JKI: Time for matrix multiplication %6.2f seconds\n", time);
 
   /*
   printf("Matrix A:\n");
